bb599e6e53c70f480b258f4c9a49ede32d8a2526
mahdi shahabedin <mahshahab@gmail.com>
2019-10-19 19:57:14 +0330

    Updates

---
+++ precache-manifest.9702dd561ebf954e7d69c248c7e13321.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b676f5d17c0a4194bfa90098c9b70f",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "0e33eeb09950113b898b",
+    "url": "/my-resume/static/css/main.a1d64c9f.chunk.css"
+  },
+  {
+    "revision": "c604bfcf8a4ce69cd151",
+    "url": "/my-resume/static/js/2.e6ed66be.chunk.js"
+  },
+  {
+    "revision": "0e33eeb09950113b898b",
+    "url": "/my-resume/static/js/main.6b8461bb.chunk.js"
+  },
+  {
+    "revision": "e69ab88e3555b895b636",
+    "url": "/my-resume/static/js/runtime-main.4511f30e.js"
+  },
+  {
+    "revision": "af25e683b9a66ddf279939e77d47ad3a",
+    "url": "/my-resume/static/media/MySQL-logo.af25e683.png"
+  },
+  {
+    "revision": "1fcb403e6a73fde60517d8ec8b31de62",
+    "url": "/my-resume/static/media/android-logo.1fcb403e.png"
+  },
+  {
+    "revision": "5cfae78ee91dd383bc9bf79f6ec2c8d8",
+    "url": "/my-resume/static/media/cisco-logo.5cfae78e.png"
+  },
+  {
+    "revision": "0143a61b91cca8691bcc38f7ca06fa4e",
+    "url": "/my-resume/static/media/doubleTop.0143a61b.png"
+  },
+  {
+    "revision": "79046a553ce2e9ea7fc1992ede20e610",
+    "url": "/my-resume/static/media/javaScript_logo.79046a55.png"
+  },
+  {
+    "revision": "8d1658b129b76f92db6db399e8a2a7d4",
+    "url": "/my-resume/static/media/php-logo.8d1658b1.png"
+  },
+  {
+    "revision": "067050f6ecc418ea7a7d2f3557683c8c",
+    "url": "/my-resume/static/media/python_logo.067050f6.png"
+  },
+  {
+    "revision": "a117bf9d9a88fc4ccf355c7ab7034dde",
+    "url": "/my-resume/static/media/react_logo.a117bf9d.png"
+  }
+]);
